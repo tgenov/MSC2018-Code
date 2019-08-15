5e8a8ebc27efa8b1a91fc04e923f6c6e4cf62306
Todor Genov <todor@subnet.co.za>
2019-08-15 13:56:48 +0200

    Change link to osf.io

--- reproduction/restore-postgres.sh
+++ reproduction/restore-postgres.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget s3://todorgenov-master-thesis-data/postgres-dump/cowrie-data.dump
+# From https://osf.io/vkcrn/
+wget https://osf.io/8wbfr/download
 DBNAME="$USERcowrielogs"
 createdb $DBNAME
 pg_restore --no-owner --role=$USER -d "$DBNAME" cowrie-data.dump
